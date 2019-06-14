787ccffe649dba120f89e3e21577312edd7f3f06
hikaru <hikaru.fujita.0903@gmail>
2019-06-14 22:44:53 +0900

    Feat: Add Error Handling

--- src/backend/axios/index.js
+++ src/backend/axios/index.js
@@ -28,26 +28,31 @@ securedAxiosInstance.interceptors.request.use(config => {
   return config
 })
 
-securedAxiosInstance.interceptors.request.use(null, error => {
-  if (error.response && error.response.config && error.reponse.status === 401) {
-    return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
-      .then(response => {
-        localStorage.csrf = response.data.csrf
-        localStorage.signedIn = true
+securedAxiosInstance.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    if (error.response && error.response.config && error.response.status === 401) {
+      return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
+        .then(response => {
+          localStorage.csrf = response.data.csrf
+          localStorage.signedIn = true
 
-        let retryConfig = error.response.config
-        retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
-        return plainAxiosInstance.request(retryConfig)
-      }).catch(error => {
-        delete localStorage.csrf
-        delete localStorage.signedIn
+          let retryConfig = error.response.config
+          retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
+          return plainAxiosInstance.request(retryConfig)
+        }).catch(error => {
+          delete localStorage.csrf
+          delete localStorage.signedIn
 
-        location.replace('/')
-        return Promise.reject(error)
-      })
-  } else {
-    return Promise.reject(error)
+          location.replace('/')
+          return Promise.reject(error)
+        })
+    } else {
+      return Promise.reject(error)
+    }
   }
-})
+)
 
 export { securedAxiosInstance, plainAxiosInstance }
