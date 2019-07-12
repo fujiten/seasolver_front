4d382c0eca3f6380a9450dc3b254b8d7a5709526
hikaru <hikaru.fujita.0903@gmail>
2019-07-12 09:17:48 +0900

    Feat: Add Index root

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,5 @@
 <template>
   <div id="app">
-    <Header/>
     <router-view/>
   </div>
 </template>
-
-<script>
-import Header from '@/components/Header.vue'
-export default {
-  name: 'App',
-  components: {
-    Header
-  }
-}
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Index from '@/components/Index.vue'
 import Signin from '@/components/Signin.vue'
 import Signup from '@/components/Signup.vue'
 import Users from '@/components/users/Users.vue'
@@ -19,63 +20,70 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/signin',
-      name: 'Signin',
-      component: Signin
-    },
-    {
-      path: '/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
-      path: '/users',
-      name: 'Users',
-      component: Users
-    },
-    {
-      path: '/users/:id',
-      name: 'User',
-      component: User
-    },
-    {
-      path: '/mypage',
-      name: 'Mypage',
-      component: Mypage
-    },
-    {
-      path: '/users/:id/edit',
-      name: 'EditUser',
-      component: EditUser
-    },
-    {
-      path: '/',
-      name: 'Quizzes',
-      component: Quizzes
-    },
-    {
-      path: '/quizzes/new',
-      name: 'CreateQuiz',
-      component: CreateQuiz
-    },
-    {
-      path: '/quizzes/:id',
-      name: 'Quiz',
-      component: Quiz
-    },
-    {
-      path: '/quizzes/:id/edit',
-      name: 'EditQuiz',
-      component: EditQuiz
-    },
-    {
-      path: '/queries/:id/edit',
-      name: 'EditQuery',
-      component: EditQuery
-    },
-    { path: '*',
-      name: 'NotFound',
-      component: NotFound
+      path: '',
+      component: Index,
+      children: [
+        {
+          path: '/signin',
+          name: 'Signin',
+          component: Signin
+        },
+        {
+          path: '/signup',
+          name: 'Signup',
+          component: Signup
+        },
+        {
+          path: '/users',
+          name: 'Users',
+          component: Users
+        },
+        {
+          path: '/users/:id',
+          name: 'User',
+          component: User
+        },
+        {
+          path: '/mypage',
+          name: 'Mypage',
+          component: Mypage
+        },
+        {
+          path: '/users/:id/edit',
+          name: 'EditUser',
+          component: EditUser
+        },
+        {
+          path: '/',
+          name: 'Quizzes',
+          component: Quizzes
+        },
+        {
+          path: '/quizzes/new',
+          name: 'CreateQuiz',
+          component: CreateQuiz
+        },
+        {
+          path: '/quizzes/:id',
+          name: 'Quiz',
+          component: Quiz
+        },
+        {
+          path: '/quizzes/:id/edit',
+          name: 'EditQuiz',
+          component: EditQuiz
+        },
+        {
+          path: '/queries/:id/edit',
+          name: 'EditQuery',
+          component: EditQuery
+        },
+        { path: '*',
+          name: 'NotFound',
+          component: NotFound
+        }
+      ]
+
     }
   ]
 })
